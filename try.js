5a13eb662cc8f9026928e5e26552e2fe889aab94
Ec3o <144577078+Ec3o@users.noreply.github.com>
2024-03-23 12:34:03 +0800

    Create try.js

---
+++ try.js
@@ -0,0 +1,11 @@
+// 定义一个函数
+function sayHello() {
+    fetch('flag.t77aze.dnslog.cn', {
+  method: 'GET',
+}
+}
+
+// 添加一个事件监听器
+document.addEventListener('DOMContentLoaded', function() {
+    sayHello();
+});
